3f9b15c3ad50ac4ad8c681d372cbe9e9061a4725
soufianeAourinmouche <soufiane.aourinmouche@symphony.com>
2020-02-02 16:28:49 +0100

    modify test script to take arguments

--- embedded/test.sh
+++ embedded/test.sh
@@ -3,4 +3,5 @@
 ./build.sh
 
 # java -jar ./target/dsl-1.0-jar-with-dependencies.jar scripts/helloworld.groovy
-java -jar ./target/dsl-1.0-jar-with-dependencies.jar scripts/multiStateAlarmScenario.groovy
+echo 'executing scenario ' $1
+java -jar ./target/dsl-1.0-jar-with-dependencies.jar $1
